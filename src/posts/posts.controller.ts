da137b01861755a15fcb0fca7c67d3bca389e662
kjuiop <copying_y@naver.com>
2026-03-24 22:11:49 +0900

    feat: ParseIntPipe post controller apply

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,7 +3,8 @@ import {
   Post,
   Body,
   Put,
-  Delete
+  Delete,
+  ParseIntPipe
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
@@ -20,14 +21,14 @@ export class PostsController {
 
   // 2) GET /posts/:id
   @Get(':id')
-  getPost(@Param('id') id: string) {
-    return this.postsService.getPostById(+id);
+  getPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.getPostById(id);
   }
 
   // 3) POST /posts
   @Post()
   postPost(
-    @Body('authorId') authorId: number,
+    @Body('authorId', ParseIntPipe) authorId: number,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
@@ -37,16 +38,16 @@ export class PostsController {
   // 4) PUT /posts/:id
   @Put(':id')
   putPost(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    return this.postsService.updatePost(+id, title, content);
+    return this.postsService.updatePost(id, title, content);
   }
 
   // 5) DELETE /posts/:id
   @Delete(':id')
-  deletePost(@Param('id') id: string) {
-    return this.postsService.deletePost(+id);
+  deletePost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.deletePost(id);
   }
 }
